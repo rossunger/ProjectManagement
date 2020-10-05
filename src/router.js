c07a65ba5f7ea89e6d5e2d87c3e8dfef8a0de186
rossunger <ross93@gmail.com>
2020-10-05 11:43:08 -0400

    Update router.js

--- src/router.js
+++ src/router.js
@@ -42,7 +42,7 @@ const router = createRouter({
 })
 router.beforeEach((to, from,next)=>{    
     let routerAuthCheck = false     
-    if(!store.state.currentUser) $store.state.currentUser = this.$store.getters.personByEmail(localStorage.getItem('current_user')) || undefined
+    if(!store.state.currentUser) store.state.currentUser = store.getters.personByEmail(localStorage.getItem('current_user')) || undefined
     if (localStorage.getItem('access_token') && localStorage.getItem('id_token') && localStorage.getItem('expires_at')){        
         let expires_at = JSON.parse(localStorage.getItem('expires_at'))
         routerAuthCheck = new Date().getTime() < expires_at
